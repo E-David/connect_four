38f04b39804592d085ddb1fd7329abef491f68cf
E-David <ericdavid365@gmail.com>
2016-04-12 18:43:08 -0500

    Readme fix

--- example/example.rb
+++
@@ -1,8 +0,0 @@
-# /example/example_game.rb
-
-require_relative "../lib/connect_four.rb"
-puts "Welcome to connect four!"
-bob = ConnectFour::Player.new("Bob","X")
-frank = ConnectFour::Player.new("Frank","O")
-players = [bob, frank]
-ConnectFour::Game.new(players).play_game
